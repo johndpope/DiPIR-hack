1e49e7a17a471b2d18f1dfdab35a137236a0037f
John Pope <jp@bellgeorge.com>
2024-09-24 17:59:42 +1000

    ok

--- models.py
+++ models.py
@@ -26,7 +26,6 @@ class EnvironmentLight(nn.Module):
         aggregated_color = colors.mean(dim=0, keepdim=True)
         return aggregated_direction, aggregated_color
 
-
 class ToneMapping(nn.Module):
     def __init__(self, num_bins=5, device='cpu'):
         super(ToneMapping, self).__init__()
@@ -37,23 +36,21 @@ class ToneMapping(nn.Module):
 
     def forward(self, x):
         x = torch.clamp(x, 0, 1)
-        device = x.device  # Get the device of input tensor x
+        device = x.device
 
         bin_edges = torch.cumsum(self.widths, 0)
-        bin_idx = torch.searchsorted(bin_edges, x)
+        bin_idx = torch.searchsorted(bin_edges.unsqueeze(0).expand(x.shape[0], -1), x.flatten())
         bin_idx = torch.clamp(bin_idx, 0, self.num_bins - 1)
         
-        widths_cumsum = torch.cat([torch.zeros(1, device=device), bin_edges[:-1]], dim=0)
-        x_low = torch.gather(widths_cumsum, 0, bin_idx)
+        widths_cumsum = torch.cat([torch.zeros(1, device=device), bin_edges[:-1]])
+        x_low = widths_cumsum[bin_idx].view_as(x)
+        x_high = (widths_cumsum + self.widths)[bin_idx].view_as(x)
+        y_low = self.heights[:-1][bin_idx].view_as(x)
+        y_high = self.heights[1:][bin_idx].view_as(x)
+        slope_low = self.slopes[:-1][bin_idx].view_as(x)
+        slope_high = self.slopes[1:][bin_idx].view_as(x)
         
-        # Proceed with the rest of the computation, ensuring all tensors are on the same device
-        x_high = x_low + torch.gather(self.widths, 0, bin_idx)
-        y_low = torch.gather(self.heights[:-1], 0, bin_idx)
-        y_high = torch.gather(self.heights[1:], 0, bin_idx)
-        slope_low = torch.gather(self.slopes[:-1], 0, bin_idx)
-        slope_high = torch.gather(self.slopes[1:], 0, bin_idx)
-        
-        t = (x - x_low) / (x_high - x_low)
+        t = (x - x_low) / (x_high - x_low + 1e-8)
         numerator = slope_low * t ** 2 + 2 * t * (1 - t)
         denominator = (slope_low + slope_high) * t ** 2 + 2 * (slope_low + slope_high - 2) * t + 2
         y = y_low + (y_high - y_low) * numerator / denominator
